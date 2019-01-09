c0f83121b86a92820a1d08442daa485d721f7887
TobiasMichael96 <tobiasmichael@mail.de>
2019-01-09 12:52:18 +0100

    Commit 1

---
+++ index.php
@@ -0,0 +1,72 @@
+<?php
+if (isset($_FILES['_files'])) {
+
+    // Checks if a config file exists, if yes the config file will be used instead of the defaults
+    if (file_exists('config.php')) {
+	include 'config.php';
+    } else {
+	$settings = array(
+	    // The folder where the file will end; do not forget the trailing slash!
+	    'upload_dir' => dirname(__FILE__) . DIRECTORY_SEPARATOR,
+
+	    // The folder where the symlink will end; do not forget the trailing slash!
+	    // You can create a symlink if the file needs to be in another folder to be shown on the webserver
+	    'symlink_dir' => '',
+
+	    // URL for the server; do not forget the trailing slash!
+	    // If you leave this empty, the URL stays the same as the page is
+	    'page_url' => autoDetectBaseUrl(),
+
+	    // Characters for the random filename
+	    'random_name_alphabet' => 'AaBbCcDdEeFfGgHhIiJjKkLlMmNnOoPpQqRrSsTtUuVvWwXxYyZz0123456789',
+	);
+    }
+
+    $imgname = $_FILES['_files']['name'];
+    $imgname_tmp = $_FILES['_files']['tmp_name'];
+
+    $extension = substr($imgname, strpos($imgname, ".") + 1);
+
+    // Generating random file name
+    $imgname = '';
+    while (strlen($imgname) < 10) {
+	$imgname .= $settings['random_name_alphabet'][mt_rand(0, strlen($settings['random_name_alphabet']) - 1)];
+    }
+    $imgname .= '.' . $extension;
+
+    if (move_uploaded_file($imgname_tmp, $settings['upload_dir'] . $imgname)) {
+	if ($settings['symlink_dir'] !== '') {
+	    if (symlink($settings['upload_dir'] . $imgname, $settings['symlink_dir'] . $imgname)) {
+		echo $settings['page_url'] . $imgname;
+	    } else {
+		echo "Error, could not create symlink!";
+	    }
+	} else {
+	    echo $settings['page_url'] . $imgname;
+	}
+    } else {
+	echo "Error, cloud not move file!";
+    }
+}
+
+// Detects base URL
+function autoDetectBaseUrl () {
+    // Detect protocol
+    $protocol = 'http';
+    if (((isset($_SERVER['HTTPS'])) && (strtolower($_SERVER['HTTPS']) == 'on')) ||
+    ((isset($_SERVER['HTTP_X_FORWARDED_PROTO'])) && (strtolower($_SERVER['HTTP_X_FORWARDED_PROTO']) == 'https'))) $protocol = 'https';
+
+    // Detect port
+    $port = getenv('SERVER_PORT');
+    if ((($port == 80) && ($protocol == 'http')) || (($port == 443) && ($protocol == 'https'))) $port = '';
+
+    // Detect server name
+    $server_name = getenv('SERVER_NAME');
+    if ($server_name === false) $server_name = 'localhost';
+
+    // Construct base URL
+    $base_url = sprintf('%s://%s%s%s', $protocol, $server_name, $port, dirname(getenv('SCRIPT_NAME'))) . DIRECTORY_SEPARATOR;
+
+    return $base_url;
+}
+?>
