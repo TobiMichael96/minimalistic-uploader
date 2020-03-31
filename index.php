f0c7bfd381b21620dddcae563bdf11b5f14f1c66
Tobias Effner <me@tobiasmichael.de>
2020-03-31 16:12:03 +0200

    reworked a lot of things in order to make this a docker container

--- index.php
+++ index.php
@@ -1,63 +1,46 @@
 <?php
-if (isset($_FILES['file'])) {
-
-    // Checks if a config file exists, if yes the config file will be used instead of the defaults
-    if (file_exists('config.php')) {
-	include 'config.php';
+if (file_exists('uploader/config.php')) {
+    include 'uploader/config.php';
+    if ($settings['symlink_dir'] !== '') {
+        $folder = $settings['symlink_dir'];
     } else {
-	$settings = array(
-	    // The folder where the file will end; do not forget the trailing slash!
-	    'upload_dir' => dirname(__FILE__) . DIRECTORY_SEPARATOR,
+        $folder = $settings['upload_dir'];
+    }
+    $extension = $settings['image_extension'];
+    $base_url = $settings['page_url'];
+} else {
+    $folder = dirname(__FILE__) . DIRECTORY_SEPARATOR;
+    $extension = 'jpg';
+    $base_url = autoDetectBaseUrl();
+}
 
-	    // The folder where the symlink will end; do not forget the trailing slash!
-	    // You can create a symlink if the file needs to be in another folder to be shown on the webserver
-	    'symlink_dir' => '',
 
-	    // URL for the server; do not forget the trailing slash!
-	    // If you leave this empty, the URL stays the same as the page is
-	    'page_url' => autoDetectBaseUrl(),
+if (isset($_GET['delete'])) {
+    unlink($_GET['delete']);
+    header('Location: ' . $base_url);
+} else {
+    $files = array();
+    $counter = 0;
 
-	    // Characters for the random filename
-	    'random_name_alphabet' => 'AaBbCcDdEeFfGgHhIiJjKkLlMmNnOoPpQqRrSsTtUuVvWwXxYyZz0123456789',
-
-	    // Mime type you want to accept
-	    'screenshot_mime_type' => IMAGETYPE_JPEG,
-	);
+    foreach (glob($folder . '*.*') as $file) {
+        if (substr($file, strpos($file, ".") + 1) == $extension) {
+            $files[$counter] = array("name" => basename($file), "time" => filemtime($file), "path" => $file);
+            ++$counter;
+        }
     }
 
-    $imgname = $_FILES['file']['name'];
-    $imgname_tmp = $_FILES['file']['tmp_name'];
-
-    $extension = substr($imgname, strpos($imgname, ".") + 1);
-
-    // Generating random file name
-    $imgname = '';
-    while (strlen($imgname) < 12) {
-	$imgname .= $settings['random_name_alphabet'][mt_rand(0, strlen($settings['random_name_alphabet']) - 1)];
-    }
-    $imgname .= '.' . $extension;
-
-    if (move_uploaded_file($imgname_tmp, $settings['upload_dir'] . $imgname)) {
-	if ($settings['symlink_dir'] !== '') {
-	    if (symlink($settings['upload_dir'] . $imgname, $settings['symlink_dir'] . $imgname)) {
-		echo $settings['page_url'] . $imgname;
-	    } else {
-		echo "Error, could not create symlink!";
-	    }
-	} else {
-	    echo $settings['page_url'] . $imgname;
-	}
-    } else {
-	echo "Error, cloud not move file!";
-    }
+    usort($files, function ($a, $b) {
+        return $b['time'] <=> $a['time'];
+    });
 }
 
 // Detects base URL
-function autoDetectBaseUrl () {
+function autoDetectBaseUrl()
+{
     // Detect protocol
     $protocol = 'http';
     if (((isset($_SERVER['HTTPS'])) && (strtolower($_SERVER['HTTPS']) == 'on')) ||
-    ((isset($_SERVER['HTTP_X_FORWARDED_PROTO'])) && (strtolower($_SERVER['HTTP_X_FORWARDED_PROTO']) == 'https'))) $protocol = 'https';
+        ((isset($_SERVER['HTTP_X_FORWARDED_PROTO'])) && (strtolower($_SERVER['HTTP_X_FORWARDED_PROTO']) == 'https'))) $protocol = 'https';
 
     // Detect port
     $port = getenv('SERVER_PORT');
@@ -68,8 +51,54 @@ function autoDetectBaseUrl () {
     if ($server_name === false) $server_name = 'localhost';
 
     // Construct base URL
-    $base_url = sprintf('%s://%s%s%s', $protocol, $server_name, $port, dirname(getenv('SCRIPT_NAME'))) . DIRECTORY_SEPARATOR;
-
-    return $base_url;
+    return sprintf('%s://%s%s%s', $protocol, $server_name, $port, dirname(getenv('SCRIPT_NAME'))) . DIRECTORY_SEPARATOR;
 }
+
 ?>
+
+<html>
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width,initial-scale=1">
+    <title>TM - Screenshots</title>
+    <link rel="icon" type="image/png" href="https://www.tobiasmichael.de/images/my-fav.png"/>
+    <!-- Latest compiled and minified CSS -->
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css">
+
+    <!-- Custom Fonts -->
+    <link rel="stylesheet" href="custom_font.css">
+</head>
+<body class="text-center">
+<div class="body-content">
+    <div class="p-3 h-100 d-flex justify-content-center">
+        <table class="table" id="table">
+            <thead class="thead-dark">
+            <tr class="d-flex">
+                <th scope="col" class="col-5">Link</th>
+                <th scope="col" class="col-5">Created on</th>
+                <th scope="col" class="col-2">Delete</th>
+            </tr>
+            </thead>
+            <tbody>
+            <?php
+            foreach ($files as $key => $value) {
+                $time = $value["time"];
+                $name = $value["name"];
+                $path = $value["path"];
+
+                echo '<tr class="d-flex">';
+                echo '<td class="col-5"><a target="_blank" rel="noopener noreferrer" href="' . $base_url . $name . '"><h5>' . substr($name, 0, strpos($name, ".")) . '</h5></a></td>';
+                echo '<td class="col-5">' . date("d F Y", $time) . ' at ' . date("H:i:s", $time) . '</td>';
+                echo '<td class="col-2"><a href="' . $base_url . 'index.php?delete=' . $path . '" class="btn btn-danger btn-sm">Delete</a></td>';
+                echo '</tr>';
+
+            }
+            ?>
+
+            </tbody>
+        </table>
+    </div>
+</div>
+
+</body>
+</html>
--- uploader/sample.config.php
+++ uploader/sample.config.php
@@ -1,11 +1,11 @@
 <?php
 $settings = array(
     // The folder where the file will end; do not forget the trailing slash!
-    'upload_dir' => '/home/user/images/',
+    'upload_dir' => '/mnt/images/',
 
     // The folder where the symlink will end; do not forget the trailing slash!
     // You can create a symlink if the file needs to be in another folder to be shown on the webserver
-    'symlink_dir' => 'var/www/html/uploads/images/',
+    'symlink_dir' => '/var/www/html/',
 
     // URL for the server; do not forget the trailing slash!
     // If you leave this empty, the URL stays the same as the page is
@@ -15,6 +15,6 @@ $settings = array(
     'random_name_alphabet' => 'AaBbCcDdEeFfGgHhIiJjKkLlMmNnOoPpQqRrSsTtUuVvWwXxYyZz0123456789',
 
     // Mime type you want to accept
-    'screenshot_mime_type' => IMAGETYPE_JPEG,
+    'image_extension' => 'jpg',
  );
 ?>
