84857ce72de0cb461009ba836eba8b72e5cb6e6c
Tobias Effner <me@tobiasmichael.de>
2020-04-02 00:20:40 +0200

    let the page refresh itself every 30 seconds

--- index.php
+++ index.php
@@ -35,6 +35,7 @@ if (isset($_GET['delete'])) {
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width,initial-scale=1">
+    <meta http-equiv="refresh" content="30" >
     <title>TM - Screenshots</title>
     <link rel="icon" type="image/icon" href="favicon.ico"/>
     <!-- Latest compiled and minified CSS -->
